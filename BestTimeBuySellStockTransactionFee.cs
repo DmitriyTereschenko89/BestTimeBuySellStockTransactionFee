a7e129a22ec268fc0520d3ea9efd5dba0da77e57
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-22 18:56:03 +0300

    Добавьте файлы проекта.

---
+++ BestTimeBuySellStockTransactionFee.cs
@@ -0,0 +1,19 @@
+﻿namespace BestTimeBuySellStockTransactionFee
+{
+	internal class BestTimeBuySellStockTransactionFee
+	{
+		public int MaxProfit(int[] prices, int fee)
+		{
+			int n = prices.Length;
+			int[] free = new int[n];
+			int[] hold = new int[n];
+			hold[0] = -prices[0];
+			for (int i = 1; i < n; ++i)
+			{
+				hold[i] = Math.Max(hold[i - 1], free[i - 1] - prices[i]);
+				free[i] = Math.Max(free[i - 1], hold[i - 1] + prices[i] - fee);
+			}
+			return free[n - 1];
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BestTimeBuySellStockTransactionFee
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			BestTimeBuySellStockTransactionFee bestTimeBuySellStockTransactionFee = new();
+            Console.WriteLine(bestTimeBuySellStockTransactionFee.MaxProfit(new int[] { 1, 3, 2, 8, 4, 9 }, 2));
+			Console.WriteLine(bestTimeBuySellStockTransactionFee.MaxProfit(new int[] { 1, 3, 7, 5, 10, 3 }, 3));
+		}
+	}
+}
